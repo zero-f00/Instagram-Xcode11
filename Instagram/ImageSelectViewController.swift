76dc180da30192ebf01b7a504ab68cea495e1315
yuuto.masamura <yuto.jl8nml@gmail.com>
2020-03-26 20:43:16 +0900

    5つのViewControllerの用意まで

---
+++ Instagram/ImageSelectViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageSelectViewController.swift
+//  Instagram
+//
+//  Created by Yuto Masamura on 2020/03/26.
+//  Copyright © 2020 yuto.masamura. All rights reserved.
+//
+
+import UIKit
+
+class ImageSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

aaa82e93022b980a538e23bd2dde70976cb27165
yuuto.masamura <yuto.jl8nml@gmail.com>
2020-03-26 20:32:33 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Yuto Masamura on 2020/03/26.
+//  Copyright © 2020 yuto.masamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

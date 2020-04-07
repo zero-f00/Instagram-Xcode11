937216b7abb4bd3ef0a41447ae0082caf186f775
yuuto.masamura <yuto.jl8nml@gmail.com>
2020-04-08 01:46:57 +0900

    コメントボタンのレイアウト実装まで

--- Instagram/PostTableViewCell.swift
+++ Instagram/PostTableViewCell.swift
@@ -16,7 +16,8 @@ class PostTableViewCell: UITableViewCell {
     @IBOutlet weak var likeLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var captionLabel: UILabel!
-
+    @IBOutlet weak var commentButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
